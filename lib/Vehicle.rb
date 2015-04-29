f1d72c3e45e9548f6d668bf1f5a274b058aed83b
Guest User <student@epicodus.com>
2015-04-29 09:26:21 -0700

    add second spec and pass tests. returns make and model of vehicle

---
+++ lib/Vehicle.rb
@@ -0,0 +1,28 @@
+class Vehicle
+  @@all_vehicle = []
+  define_method(:initialize) do |make, modelvehicle, year|
+    @make = make
+    @model = modelvehicle
+    @year = year
+  end
+
+  define_singleton_method(:all) do
+    @@all_vehicle
+  end
+
+  define_method(:save) do
+    @@all_vehicle.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@all_vehicle = []
+  end
+
+  define_method(:make) do
+    @make
+  end
+
+  define_method(:modelvehicle) do
+    @modelvehicle
+  end
+end
--- spec/Vehicle_spec.rb
+++ spec/Vehicle_spec.rb
@@ -1,5 +1,5 @@
 require('rspec')
-require('Vehicle')
+require('vehicle')
 
 describe(Vehicle) do
   before() do
@@ -11,6 +11,14 @@ describe(Vehicle) do
       test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
       test_vehicle.save()
       expect(test_vehicle.make()).to(eq("Toyota"))
+    end
+  end
+
+  describe("#modelvehicle") do
+    it("returns the make of the vehicle") do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      test_vehicle.save()
+      expect(test_vehicle.modelvehicle()).to(eq("Prius"))
     end
   end
 end
